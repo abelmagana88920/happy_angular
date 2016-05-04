02cd3345c88dd247b1bab9e92bcb589b7b2c1454
abelmagana88920 <abelmagana88920@gmail.com>
2016-05-04 17:28:24 +0800

    Menu navigating fixed 70%

--- happyfresh/bower_components/miscellaneous/jscss.js
+++ happyfresh/bower_components/miscellaneous/jscss.js
@@ -4,7 +4,7 @@
             document.getElementById('menu').style.display = 'block';
             document.getElementById('wrapper').style.margin = '0 0 0 180px';
 
-           
+            
         });
 
  
@@ -12,12 +12,40 @@
 
          document.getElementById('show-menu').addEventListener('blur', function () {
 
-           setTimeout(function() {
-                document.getElementById('menu').style.display = 'none';
-                document.getElementById('wrapper').style.margin = '0 0 0 0';
-           }, 300); //delay 0.5 sec	
+                
+                        setTimeout(function() {
+                            document.getElementById('menu').style.display = 'none';
+                            document.getElementById('wrapper').style.margin = '0 0 0 0';
+                       }, 300); 
+                  
+               
+               /*setTimeout(function() {
+                    document.getElementById('menu').style.display = 'none';
+                    document.getElementById('wrapper').style.margin = '0 0 0 0';
+               }, 300); //delay 0.5 sec	*/
           
         });
+
+
+        
+
+        function menublur() {
+                 setTimeout(function() {
+                            document.getElementById('menu').style.display = 'none';
+                            document.getElementById('wrapper').style.margin = '0 0 0 0';
+               }, 300); 
+
+         };
+
+
+
+        function menufocus() {
+                 setTimeout(function() { 
+                      document.getElementById('menu').style.display = 'block';
+                      document.getElementById('wrapper').style.margin = '0 0 0 180px';
+                 }, 300);
+
+         };
 
 
       
